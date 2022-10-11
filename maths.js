615f28a866c844635a29a5dac84272de8ed6cf8e
LeThom <sms202112@outlook.com>
2022-10-11 12:19:38 +0100

    init commit

---
+++ maths.js
@@ -0,0 +1,6 @@
+module.exports = {
+    sum: (a, b) => a + b,
+    substract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+}
---
+++ maths.test.js
@@ -0,0 +1,8 @@
+const {sum} = require("./maths.js")
+const {ex}
+describe("maths tests", () => { //describe = set of tests
+    it("should equal 2", () => {
+        sum(1, 1) 
+
+    }) //it = single test
+});
