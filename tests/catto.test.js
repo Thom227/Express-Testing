7cc42045ba0a3ea19563a3e6b9f3d070bc14aa6b
LeThom <sms202112@outlook.com>
2022-10-12 16:54:10 +0100

    latest modifications

--- tests/catto.test.js
+++ tests/catto.test.js
@@ -1,10 +1,10 @@
-const {catto} = require("./catto.js");
+const {catto} = require("../catto.js");
 const {expect} = require("chai");
 
 describe("catto test", () => {
     it("should exclude num", () => {
         expect(catto(6)).to.equal(i!=6);
     })
-    
+
 
 });
